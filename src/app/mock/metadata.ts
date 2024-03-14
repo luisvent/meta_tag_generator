0045285ce8af6467c1631966fc732d3f002999f4
Luis Ventura <luis.ventura005@gmail.com>
2024-03-13 22:58:49 -0400

    - improved ui for share social cards

--- src/app/mock/metadata.ts
+++ src/app/mock/metadata.ts
@@ -6,7 +6,7 @@ export const mockMetadata: Metadata = {
   title: 'Meta Tags / OG Generator - Create and Preview ',
   author: 'Luis Ventura',
   url: 'https://metatags.lv-apps.com',
-  favUrl: 'https://luisvent.com/assets/favicon.ico',
+  favUrl: 'https://metatags.lv-apps.com/assets/favicon.ico',
   description: 'Generate the optimal Open Graph tags (OG tags) for your webpage.  See a live preview of how your webpage will look when shared on social media platforms like Facebook and Twitter.',
   keywords: ['meta', 'tags', 'metatags', 'og', 'facebook', 'twitter', 'html'],
   allowRobots: true,
